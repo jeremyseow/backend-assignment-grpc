6ee18c9d420b29b16dc8944bdf3d7766ed3ed4a1
Jeremy Seow <jeremyseow984@gmail.com>
2022-02-06 15:39:24 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/jeremyseow/backend-assignment-grpc/config"
+	"github.com/jeremyseow/backend-assignment-grpc/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedEventServiceServer
+}
+
+func main() {
+	listener, err := net.Listen("tcp", config.Conf.GrpcPort)
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterEventServiceServer(grpcServer, &server{})
+	reflection.Register(grpcServer)
+
+	if e := grpcServer.Serve(listener); e != nil {
+		panic(err)
+	}
+}
+
+func (s *server) SendEvent(_ context.Context, request *pb.EventRequest) (*pb.EventResponse, error) {
+	result := request.String()
+
+	return &pb.EventResponse{Result: result}, nil
+}
